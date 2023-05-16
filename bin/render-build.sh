6c21987e6da6d51016315419f2a9f5053f3b2c1a
h.umeki <h.umeki@takionsd.com>
2023-05-17 00:33:18 +0900

    render関連のコミット

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,7 @@
 set -o errexit
 
 bundle install
-bundle exec rails db:migrate
+#bundle exec rails db:migrate
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
 bundle exec rails db:seed
 bundle exec rails assets:precompile
